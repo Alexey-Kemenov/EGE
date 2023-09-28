8d6e1a99ea41f9968d4981c77ef90b1c5156ca36
Андрей Боев <a.boev.it@gmail.com>
2023-09-28 13:32:04 +0300

    Проверка ДЗ

--- 2/48450.py
+++ 2/48450.py
@@ -4,5 +4,6 @@ for x in range(2):
         for z in range(2):
             for w in range(2):
                 f = (w <= (y == z)) and (y == (z <= x))
+                # Если f равно false и среди переменных ровно одна единица или f равно true
                 if (not f and ([x, y, z, w].count(1) == 1)) or f:
                     print(x, y, z, w, int(f))
