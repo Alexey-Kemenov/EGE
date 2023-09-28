b3cca124fcc2ebbc163be029262b489cfb14f04b
Андрей Боев <a.boev.it@gmail.com>
2023-09-28 13:25:06 +0300

    Проверка ДЗ

--- 2/48450.py
+++ 2/48450.py
@@ -4,4 +4,5 @@ for x in range(2):
         for z in range(2):
             for w in range(2):
                 f = (w <= (y == z)) and (y == (z <= x))
-                print(x, y, z, w, f)
+                if (not f and ([x, y, z, w].count(1) == 1)) or f:
+                    print(x, y, z, w, int(f))
