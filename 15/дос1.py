d371b16701dca5daad32fe1b704edd425ba55f81
Андрей Боев <a.boev.it@gmail.com>
2024-04-12 13:14:59 +0300

    Задание 15

--- 15/дос1.py
+++ 15/дос1.py
@@ -1,9 +1,10 @@
+min_l = 10000
 for Aa in range(200):
     for Ab in range(200):
         f = True
-        for x in range(500):
-            if not ((x in range(47, 116)) <= (
-                    (x not in range(Aa, Ab+1)) and (x in range(24, 91))) <= (x not in range(47, 116))):
+        for x in range(200):
+            if not ((x in range(47, 116)) <= (((x not in range(Aa, Ab+1)) and (x in range(24, 91))) <= (x not in range(47, 116)))):
                 f = False
-    if f:
-        print(Aa, Ab)
+        if f:
+            min_l = min((Ab-Aa), min_l)
+print(min_l)
